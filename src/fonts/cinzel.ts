5d6fd730e0ab79f21e07fc39634753563f2b3cec
Rohan Gayen <rohangayen@gmail.com>
2023-02-17 18:02:55 +0530

    chore: import fonts
    Cinzel and Lato from Google fonts

---
+++ src/fonts/cinzel.ts
@@ -0,0 +1,5 @@
+import { Cinzel } from '@next/font/google';
+
+const cinzel = Cinzel({ subsets: ['latin'], weight: ['700'] });
+
+export default cinzel;
---
+++ src/fonts/lato.ts
@@ -0,0 +1,5 @@
+import { Lato } from '@next/font/google';
+
+const lato = Lato({ subsets: ['latin'], weight: ['400'] });
+
+export default lato;
