671e9bb356aa75114e3092b6df52f253d1d4c519
Rohan Gayen <rohangayen@gmail.com>
2023-02-23 19:41:09 +0530

    chore: add patterns for next/image

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
 	reactStrictMode: true,
 	compiler: {
 		styledComponents: true
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'i.dummyjson.com',
+				port: ''
+			}
+		]
 	}
 };
 
